5515baa8fbb91681b5a2e63deb104697c448d785
aravind kumar <aravindkumarreddy66@gmail.com>
2025-10-31 16:57:10 +0530

    terraform-vpc

--- outputs.tf
+++ outputs.tf
@@ -2,10 +2,6 @@
     value = module.vpc.vpc_id
 } 
 
-/* output "public_subnet_ids" {
-    value = module.vpc.public_subnet_ids
-}  */
-
 output "public_subnet_ids" {
   value = module.vpc.public_subnet_ids
 }
